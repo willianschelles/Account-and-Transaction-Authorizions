7cc355bcf4f16884aef5458c42bb4212b54175e8
Willian Schelles Sabino <willianschelles@gmail.com>
2021-03-26 20:43:43 -0300

    add happy path to create Account proccess

--- lib/account.ex
+++ lib/account.ex
@@ -9,4 +9,20 @@ defmodule Account do
           available_limit: integer()
         }
   defstruct active_card: nil, available_limit: nil
+
+  @spec create(keyword()) :: {:error, any} | {:ok, pid}
+  @doc """
+  Starts a new account in a separated process (keeping state, not shared memory).
+  """
+  def create(state) do
+    active_card = Keyword.get(state, :active_card)
+    available_limit = Keyword.get(state, :available_limit)
+
+    Agent.start_link(
+      fn ->
+        %Account{active_card: active_card, available_limit: available_limit}
+      end,
+      name: Account
+    )
+  end
 end
--- test/account_test.exs
+++ test/account_test.exs
@@ -9,4 +9,12 @@ defmodule AccountTest do
       assert %Account{active_card: nil, available_limit: nil} = %Account{}
     end
   end
+
+  describe "create/1" do
+    test "creates an Account process with given state and returns its PID" do
+      account_attrs = [active_card: true, available_limit: 100]
+      {:ok, account_pid} = Account.create(account_attrs)
+      assert is_pid(account_pid)
+    end
+  end
 end
