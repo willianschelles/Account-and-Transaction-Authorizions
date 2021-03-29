e5f1c5bd3c8079128c1e3ddac56845f4916c9b46
Willian Schelles Sabino <willianschelles@gmail.com>
2021-03-29 06:16:40 -0300

    remove comments

--- test/authorizer_test.exs
+++ test/authorizer_test.exs
@@ -12,16 +12,6 @@ defmodule AuthorizerTest do
       assert %Output{account_state: ^expected_acount, violations: ^expected_violations} =
                Authorizer.run(@account_payload)
 
-      # DynamicSupervisor.terminate_child(Authorizer.DynamicSupervisor, account_pid)
     end
   end
 end
-
-# input
-# {"account": {"active-card": true, "available-limit": 100}} ...
-# {"account": {"active-card": true, "available-limit": 350}}
-
-# output
-# {"account": {"active-card": true, "available-limit": 100}, "violations": []}
-# ...
-# {"account": {"active-card": true, "available-limit": 100}, "violations": ["account-already-initialized" ]}
